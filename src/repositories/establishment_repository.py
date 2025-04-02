6662299ac6b8032c42ee7f7f14a8ffc690eae7a7
victortsrodrigues <victadeu@gmail.com>
2025-04-02 11:40:51 -0300

    Update to query single table

--- src/repositories/establishment_repository.py
+++ src/repositories/establishment_repository.py
@@ -8,25 +8,49 @@ class EstablishmentRepository:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
+    # def check_establishment(self, ibge_cnes):
+    #     try:
+    #         # Check serv159152 table (equivalent to valid CNES)
+    #         with settings.engine.connect() as conn:
+    #             result = conn.execute(
+    #                 text('SELECT COUNT(*) FROM serv159152 WHERE "CO_UNIDADE" = :val'),
+    #                 {"val": ibge_cnes}
+    #             ).fetchone()
+    #             if result[0] > 0:
+    #                 return True
+                
+    #             # Check all_estab_serv_class table (equivalent to general CNES)
+    #             result = conn.execute(
+    #                 text('SELECT COUNT(*) FROM all_estab_serv_class WHERE "CO_UNIDADE" = :val'),
+    #                 {"val": ibge_cnes}
+    #             ).fetchone()
+
+    #             return False if result[0] > 0 else None
+        
+    #     except SQLAlchemyError as e:
+    #         self.logger.error(f"Database error: {str(e)}")
+    #         return None
+    
     def check_establishment(self, ibge_cnes):
         try:
             # Check serv159152 table (equivalent to valid CNES)
             with settings.engine.connect() as conn:
-                result = conn.execute(
-                    text('SELECT COUNT(*) FROM serv159152 WHERE "CO_UNIDADE" = :val'),
-                    {"val": ibge_cnes}
-                ).fetchone()
-                if result[0] > 0:
-                    return True
-                
-                # Check all_estab_serv_class table (equivalent to general CNES)
+                # Check if estab exists in all_estab_serv_class table
                 result = conn.execute(
                     text('SELECT COUNT(*) FROM all_estab_serv_class WHERE "CO_UNIDADE" = :val'),
                     {"val": ibge_cnes}
                 ).fetchone()
-
-                return False if result[0] > 0 else None
-        
+                estab_exists_in_all_estab_serv_class = result[0] > 0
+                if estab_exists_in_all_estab_serv_class:
+                    result = conn.execute(
+                        text('SELECT COUNT(*) FROM all_estab_serv_class WHERE "CO_UNIDADE" = :val AND "CO_SERVICO" IN (159, 152)'),
+                        {"val": ibge_cnes}
+                    ).fetchone()
+                    return True if result[0] > 0 else False
+                    
+                else:
+                    return None
+                        
         except SQLAlchemyError as e:
             self.logger.error(f"Database error: {str(e)}")
             return None
